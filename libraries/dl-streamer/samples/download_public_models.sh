4ecaf1e477ce78821044c75fcc3c12409f7786be
Michal Holownia <michal.holownia@intel.com>
2025-09-03 10:25:35 +0200

    download models fix

--- libraries/dl-streamer/samples/download_public_models.sh
+++ libraries/dl-streamer/samples/download_public_models.sh
@@ -364,6 +364,7 @@ if [ "$MODEL" == "yolox-tiny" ] || [ "$MODEL" == "yolo_all" ] || [ "$MODEL" == "
   if [[ ! -f "$DST_FILE1" || ! -f "$DST_FILE2" ]]; then
     cd "$MODELS_PATH"
     echo "Downloading and converting: ${MODEL_DIR}"
+    source "$VENV_DIR/bin/activate"
     omz_downloader --name "$MODEL_NAME"
     omz_converter --name "$MODEL_NAME"
     cd "$MODEL_DIR"
@@ -387,6 +388,7 @@ if [ "$MODEL" == "yolox_s" ] || [ "$MODEL" == "yolo_all" ] || [ "$MODEL" == "all
     mkdir -p "$MODEL_DIR/FP32"
     cd "$MODEL_DIR"
     curl -O -L https://github.com/Megvii-BaseDetection/YOLOX/releases/download/0.1.1rc0/yolox_s.onnx
+    source "$VENV_DIR/bin/activate"
     ovc yolox_s.onnx --compress_to_fp16=True
     mv yolox_s.xml "$MODEL_DIR/FP16"
     mv yolox_s.bin "$MODEL_DIR/FP16"
@@ -411,6 +413,7 @@ export_yolov5_model() {
     echo "Downloading and converting: ${model_path}"
     mkdir -p "$model_path"
     cd "$model_path"
+    source "$VENV_DIR/bin/activate"
 
     $PYTHON - <<EOF
 import os
@@ -498,6 +501,7 @@ for MODEL_NAME in "${YOLOv5_MODELS[@]}"; do
       cp -r "$REPO_DIR" yolov5
       cd yolov5
       curl -L -O "https://github.com/ultralytics/yolov5/releases/download/v7.0/${MODEL_NAME}.pt"
+      source "$VENV_DIR/bin/activate"
 
       $PYTHON export.py --weights "${MODEL_NAME}.pt" --include openvino --img-size 640 --dynamic
       $PYTHON - <<EOF "${MODEL_NAME}"
@@ -557,6 +561,7 @@ if [ "$MODEL" == "yolov7" ] || [ "$MODEL" == "yolo_all" ] || [ "$MODEL" == "all"
   DST_FILE2="$MODEL_DIR/FP32/$MODEL_NAME.xml"
 
   if [[ ! -f "$DST_FILE1" || ! -f "$DST_FILE2" ]]; then
+    source "$VENV_DIR/bin/activate"
     pip install --no-cache-dir onnx
     pip install --no-cache-dir --index-url https://download.pytorch.org/whl/cpu torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1  || handle_error $LINENO
     mkdir -p "$MODEL_DIR"
@@ -594,6 +599,7 @@ export_yolo_model() {
     echo "Downloading and converting: ${MODEL_DIR}"
     mkdir -p "$MODEL_DIR"
     cd "$MODEL_DIR"
+    source "$VENV_DIR/bin/activate"
 
     $PYTHON - <<EOF "$MODEL_NAME" "$MODEL_TYPE"
 from ultralytics import YOLO
@@ -738,6 +744,7 @@ if [[ "$MODEL" == "centerface" ]] || [[ "$MODEL" == "all" ]]; then
     cd "$MODEL_DIR"
     git clone https://github.com/Star-Clouds/CenterFace.git
     cd CenterFace/models/onnx
+    source "$VENV_DIR/bin/activate"
     ovc centerface.onnx --input "[1,3,768,1280]"
     mv centerface.xml "$MODEL_DIR"
     mv centerface.bin "$MODEL_DIR"
@@ -783,6 +790,7 @@ if [ "$MODEL" == "hsemotion" ] || [ "$MODEL" == "all" ]; then
     cd "$MODEL_DIR"
     git clone https://github.com/av-savchenko/face-emotion-recognition.git
     cd face-emotion-recognition/models/affectnet_emotions/onnx
+    source "$VENV_DIR/bin/activate"
 
     ovc enet_b0_8_va_mtl.onnx --input "[16,3,224,224]"
     mkdir "$MODEL_DIR/FP16/"
@@ -828,6 +836,7 @@ for MODEL_NAME in "${CLIP_MODELS[@]}"; do
       IMAGE_PATH=car.png
       curl -L -o $IMAGE_PATH $IMAGE_URL
       echo "Image downloaded to $IMAGE_PATH"
+      source "$VENV_DIR/bin/activate"
       $PYTHON - <<EOF "$MODEL_NAME" "$IMAGE_PATH"
 from transformers import CLIPProcessor, CLIPVisionModel
 import PIL
@@ -884,6 +893,7 @@ if [[ "$MODEL" == "deeplabv3" ]] || [[ "$MODEL" == "all" ]]; then
   if [[ ! -f "$DST_FILE1" || ! -f "$DST_FILE2" ]]; then
     cd "$MODELS_PATH"
     echo "Downloading and converting: ${MODEL_DIR}"
+    source "$VENV_DIR/bin/activate"
     omz_downloader --name "$MODEL_NAME"
     omz_converter --name "$MODEL_NAME"
     cd "$MODEL_DIR"
