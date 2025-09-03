fd49332c31192eb8156a4d4b971b55202351fb2a
Holownia, Michal <michal.holownia@intel.com>
2025-09-03 14:36:40 +0200

    fix pytest

--- libraries/dl-streamer/tests/unit_tests/tests_gstgva/test_pipeline_custom_preproc.py
+++ libraries/dl-streamer/tests/unit_tests/tests_gstgva/test_pipeline_custom_preproc.py
@@ -56,14 +56,14 @@ appsrc name=mysrc
 
 C_VAAPI_PIPELINE_STR = f"""
 appsrc name=mysrc
-! jpegparse ! vaapijpegdec ! video/x-raw
+! jpegparse ! vaapijpegdec ! video/x-raw(memory:VASurface)
 ! gvaclassify inference-region=full-frame pre-process-backend=vaapi device=CPU model={C_MODEL_PATH} model-proc={C_MODEL_PROC_PATH}
 ! appsink name=mysink emit-signals=true sync=false
 """
 
 C_VAAPI_SURFACE_SHARING_PIPELINE_STR = f"""
 appsrc name=mysrc
-! jpegparse ! vaapijpegdec ! video/x-raw(memory:VAMemory)
+! jpegparse ! vaapijpegdec ! video/x-raw(memory:VASurface)
 ! gvaclassify inference-region=full-frame pre-process-backend=vaapi-surface-sharing device=GPU model={C_MODEL_PATH} model-proc={C_MODEL_PROC_PATH}
 ! appsink name=mysink emit-signals=true sync=false
 """
