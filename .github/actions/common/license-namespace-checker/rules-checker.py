bf5c9dcef81ec6d64567587033c75a1240a57fdd
nszczygl9 <118973656+nszczygl9@users.noreply.github.com>
2025-10-31 09:36:03 +0100

    [DLStreamer Pipeline Server] Modify license headers action, code owners and update flask-cors (#1141)

--- .github/actions/common/license-namespace-checker/rules-checker.py
+++ .github/actions/common/license-namespace-checker/rules-checker.py
@@ -15,23 +15,29 @@ from functools import reduce
 
 COPYRIGHT_YEAR = "2025"
 
-def check_copyright_year(copyright_pattern):
+def check_copyright_year(copyright_patterns):
     def check(content):
-        match = re.search(copyright_pattern[0], content, re.DOTALL)
-        if match and COPYRIGHT_YEAR not in match.group(1):
-            return f"Year {COPYRIGHT_YEAR} is missing" 
+        for i, pattern in enumerate(copyright_patterns):
+            match = re.search(pattern[0], content, re.DOTALL)
+            if match:
+                if COPYRIGHT_YEAR not in match.group(1):
+                    return f"Year {COPYRIGHT_YEAR} is missing" 
+                else:
+                    print(f"DEBUG: Year {COPYRIGHT_YEAR} is present")
         return None
     return check
-        
-def line_length_check(string):
-    if len(string) > 120:
-        return "Line string length is great than 120"
 
 def regular_expression_check(regexp, error_msg, result_checker, content):
     re_check = re.compile(regexp, re.DOTALL)
     result = re_check.search(content)
     if result_checker(result):
         return error_msg
+
+def check_any_copyright_header(copyright_patterns, content):
+    for pattern in copyright_patterns:
+        if not regular_expression_check(pattern[0], pattern[1], pattern[2], content):
+            return None
+    return "There is no valid copyright header (MIT or Apache v2)"
 
 def process_file(file, rulename, checkers):
     # Whole content check
@@ -81,24 +87,41 @@ def process_files(file_types_descs, files):
         list(itertools.chain.from_iterable(result)),
         True)
 
-def init ():
-    c_copyright = (r'''\/\*+\s*\r?''' +
-                   r'''\* Copyright \(C\) (.*) Intel Corporation\s*\r?''' +
-                   r'''\*\s*\r?''' +
-                   r'''\* SPDX-License-Identifier: MIT\s*\r?''' +
-                   r'''( \*[^\n\r]*\s*\r?)*''' # it is for case where there is presented additional information
-                   r''' \*+\/''',
-                  "There is no C style copyright header",
-                  lambda x: not x)
-
-    sharp_copyright = (r'''# ==============================================================================\s*\r?''' +
-                       r'''# Copyright \(C\) (.*) Intel Corporation\s*\r?''' +
-                       r'''#\s*\r?''' +
-                       r'''# SPDX-License-Identifier: MIT\s*\r?''' +
-                       r'''# ==============================================================================\s*\r?''',
-                      "There is no sharped copyright header",
+def init():
+    c_copyright_mit = (r'''\/\*+\s*\r?''' +
+                       r'''\* Copyright \(C\) (.*) Intel Corporation\s*\r?''' +
+                       r'''\*\s*\r?''' +
+                       r'''\* SPDX-License-Identifier: MIT\s*\r?''' +
+                       r'''( \*[^\n\r]*\s*\r?)*''' +
+                       r''' \*+\/''',
+                      "There is no C style MIT copyright header",
                       lambda x: not x)
 
+    c_copyright_apache = (r'''\/\*+\s*\r?''' +
+                         r'''\* Apache v2 license\s*\r?''' +
+                         r'''\* Copyright \(C\) (.*) Intel Corporation\s*\r?''' +
+                         r'''\* SPDX-License-Identifier: Apache-2\.0\s*\r?''' +
+                         r'''( \*[^\n\r]*\s*\r?)*''' +
+                         r''' \*+\/''',
+                        "There is no C style Apache v2 copyright header",
+                        lambda x: not x)
+
+
+    sharp_copyright_mit = (r'''# ==============================================================================\s*\r?''' +
+                          r'''# Copyright \(C\) (.*) Intel Corporation\s*\r?''' +
+                          r'''#\s*\r?''' +
+                          r'''# SPDX-License-Identifier: MIT\s*\r?''' +
+                          r'''# ==============================================================================\s*\r?''',
+                         "There is no sharp MIT copyright header",
+                         lambda x: not x)
+
+    sharp_copyright_apache = (r'''#\s*\r?''' +
+                             r'''# Apache v2 license\s*\r?''' +
+                             r'''# Copyright \(C\) (.*) Intel Corporation\s*\r?''' +
+                             r'''# SPDX-License-Identifier: Apache-2\.0\s*\r?''' +
+                             r'''#\s*\r?''',
+                            "There is no sharp Apache v2 copyright header",
+                            lambda x: not x)
 
     using_in_header = (
         "using namespace [^;]*;",
@@ -110,12 +133,17 @@ def init ():
         "There is windows line endings in header",
         lambda x: x)
 
-    c_copyright_exist = functools.partial(regular_expression_check, *c_copyright)
-    sharp_copyright_exist = functools.partial(regular_expression_check, *sharp_copyright)
+    def c_copyright_exist(content):
+        return check_any_copyright_header([c_copyright_mit, c_copyright_apache], content)
+
+    def sharp_copyright_exist(content):
+        return check_any_copyright_header([sharp_copyright_mit, sharp_copyright_apache], content)
+
     using_in_header_exist = functools.partial(regular_expression_check, *using_in_header)
     windows_line_ending_exist = functools.partial(regular_expression_check, *windows_line_endings)
-    year_check_in_c_copyright = check_copyright_year(c_copyright)
-    year_check_in_sharp_copyright = check_copyright_year(sharp_copyright)
+    
+    year_check_in_c_copyright = check_copyright_year([c_copyright_mit, c_copyright_apache])
+    year_check_in_sharp_copyright = check_copyright_year([sharp_copyright_mit, sharp_copyright_apache])
 
     # Checkers configuration
     # (Rule name, File name pattern,  whole file checkers,     line-by-line checkers)
