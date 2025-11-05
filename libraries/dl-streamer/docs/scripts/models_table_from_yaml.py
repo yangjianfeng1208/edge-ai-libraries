c5280df27e0f8fde687cf11cd8d1afedf3678864
marcin-wadolkowski <marcin.wadolkowski@intel.com>
2025-11-05 14:07:49 +0100

    [DLStreamer] fixed some pylint problems, part2

--- libraries/dl-streamer/docs/scripts/models_table_from_yaml.py
+++ libraries/dl-streamer/docs/scripts/models_table_from_yaml.py
@@ -12,9 +12,8 @@ from argparse import ArgumentParser
 import json
 from jsonschema import validate
 
-dldt_str = "dl" + "dt"
-openvino_str = "open" + "vino"
-dlstreamer_name = "Deep " + "Learning" + " Streamer"
+OPENVINO_STR = "open" + "vino"
+DLSTREAMER_NAME = "Deep " + "Learning" + " Streamer"
 
 parser = ArgumentParser(add_help=False)
 _args = parser.add_argument_group("Options")
@@ -38,42 +37,19 @@ with open(args.verified_models) as f:
     verified_models = json.load(f)
 
 # write .rst
-with open(args.output, "w") as f:
+with open(args.output, "w", encoding="utf-8") as f:
     f.write("Supported Models\n")
     f.write("================\n")
     f.write("\n")
     f.write("This page contains the table of models supported by Deep Learning Streamer.")
     f.write("\n")
-    f.write(
-        "Each model has a link (under the model name) to the original documentation with download instructions.\n"
-    )
+    f.write("Each model has a link (under the model name) to the original ")
+    f.write("documentation with download instructions.\n")
     f.write("\n")
-    f.write(
-        "Most models are from `OpenVINO™ Open Model Zoo <https://github.com/openvinotoolkit/open_model_zoo/>`__\n"
-    )
+    f.write("Most models are from `OpenVINO™ Open Model Zoo ")
+    f.write("<https://github.com/openvinotoolkit/open_model_zoo/>`__\n")
     f.write("but some of them come from other sources.\n")
     f.write("\n")
-    """
-    f.write('Abbreviations used in the table\n')
-    f.write('--------------------------------\n')
-    f.write('.. list-table::\n')
-    f.write('    :header-rows: 1\n')
-    f.write('\n')
-    f.write('    * - Abbreviation\n')
-    f.write('      - Description\n')
-    f.write('    * - {0}\n'.format(openvino_str))
-    f.write('      - `OpenVINO™ toolkit <https://docs.openvino.ai/>`__ - as model file format ( .xml +  .bin) and inference backend in {0}\n'.format(dlstreamer_name))
-    f.write('    * - pytorch\n')
-    f.write('      - `PyTorch* framework <https://pytorch.org>`__ - as model file format and inference backend in {0}\n'.format(dlstreamer_name))
-    f.write('    * - tf\n')
-    f.write('      - `TensorFlow* framework <https://www.tensorflow.org>`__ - as model file format and inference backend in {0}\n'.format(dlstreamer_name))
-    f.write('    * - onnx\n')
-    f.write('      - `ONNX <https://onnx.ai>`__ - Open Neural Network Exchange file format\n')
-    f.write('    * - caffe\n')
-    f.write('      - `Caffe* framework <https://caffe2.ai/>`__ - as model file format\n')
-    f.write('    * - dlstreamer\n')
-    f.write('      - {0}\n'.format(dlstreamer_name))
-    """
     f.write("\n")
     f.write("Models Table\n")
     f.write("----------------\n")
@@ -87,7 +63,7 @@ with open(args.output, "w") as f:
     # f.write('      - Source Repo\n')
     # f.write('      - Available Format(s)\n')
     f.write("      - GFLOPs\n")
-    # f.write('      - {0} support\n'.format(dlstreamer_name))
+    # f.write('      - {0} support\n'.format(DLSTREAMER_NAME))
     # f.write('      - Open VINO™ support\n')
     # f.write('      - Py Torch* support\n')
     # f.write('      - Tensor Flow* support\n')
@@ -105,30 +81,31 @@ with open(args.output, "w") as f:
 
         name_s = name.replace("torchvision.models.detection.", "torchvision.models.detection. ")
         n = n + 1
-        f.write("    * - {0}\n".format(n))
-        f.write("      - {0}\n".format(string.capwords(m.get("task_type", "").replace("_", " "))))
-        f.write("      - `{0} <{1}>`__\n".format(name_s, m.get("readme", "")))
+        task_type = string.capwords(m.get("task_type", "").replace("_", " "))
+        readme = m.get("readme", "")
+        gflops = re.split(r"[^0-9\.]", m.get("GFLOPs", m.get("GFlops", " ")))[0]
+        f.write(f"    * - {n}\n")
+        f.write(f"      - {task_type}\n")
+        f.write(f"      - `{name_s} <{readme}>`__\n")
         # f.write('      - {0}\n'.format(m.get('source', '')))
         # f.write('      - {0}\n'.format(m.get('format', '')))
-        f.write(
-            "      - {0}\n".format(re.split(r"[^0-9\.]", m.get("GFLOPs", m.get("GFlops", " ")))[0])
-        )
+        f.write(f"      - {gflops}\n")
         # f.write('      - {0}\n'.format(m.get('dlstreamer_support', '')))
         # f.write('      - {0}\n'.format(m.get('openvino_devices', '?')))
         # f.write('      - {0}\n'.format(m.get('pytorch_devices', '')))
         # f.write('      - {0}\n'.format(m.get('tf_devices', '')))
         labels_file = m.get("labels-file", None)
         if labels_file:
-            f.write("      - `{0} <{1}>`__\n".format(os.path.basename(labels_file), labels_file))
+            path_labels_file = os.path.basename(labels_file)
+            f.write(f"      - `{path_labels_file} <{labels_file}>`__\n")
         else:
-            # f.write('      - {0}\n'.format(m.get('datasets', [{}])[0].get('name','').replace('-',' ').replace('_',' ')))
             f.write("      -\n")
         model_proc = m.get("model-proc", None)
         if model_proc:
             if model_proc[0:4] != "http":
-                f.write("      - {0}\n".format(model_proc))
+                f.write(f"      - {model_proc}\n")
             else:
-                f.write("      - `{0} <{1}>`__\n".format("model-proc", model_proc))
+                f.write(f"      - `model-proc <{model_proc}>`__\n")
         else:
             f.write("      -\n")
         # f.write('      - {0}\n'.format(m.get('omz_demo', '')))
@@ -143,15 +120,13 @@ with open(args.output, "w") as f:
         if (t1 == "") or (t2 == ""):
             f.write("      -\n")
         else:
-            f.write("      - `{0} <{1}>`__\n".format(t1, t2))
+            f.write(f"      - `{t1} <{t2}>`__\n")
 
     f.write("\n")
     f.write("Legal Information\n")
     f.write("-------------------\n")
-    f.write(
-        "PyTorch, TensorFlow, Caffe, Keras, MXNet are trademarks or brand names of their respective owners.\n"
-    )
-    f.write(
-        "All company, product and service names used in this website are for identification purposes only.\n"
-    )
+    f.write("PyTorch, TensorFlow, Caffe, Keras, MXNet are trademarks ")
+    f.write("or brand names of their respective owners.\n")
+    f.write("All company, product and service names used in this ")
+    f.write("website are for identification purposes only.\n")
     f.write("Use of these names,trademarks and brands does not imply endorsement.\n")
--- libraries/dl-streamer/src/gst/python/python_object_association.py
+++ libraries/dl-streamer/src/gst/python/python_object_association.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # ==============================================================================
-# Copyright (C) 2023 Intel Corporation
+# Copyright (C) 2023-2025 Intel Corporation
 #
 # SPDX-License-Identifier: MIT
 # ==============================================================================
