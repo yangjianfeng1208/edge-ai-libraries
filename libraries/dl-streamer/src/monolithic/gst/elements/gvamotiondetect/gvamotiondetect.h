d6ce69adf9b09b5bc25ae913167fb91791fbefde
Maciej Smiatacz <maciej.smiatacz@intel.com>
2025-11-19 14:30:59 +0100

    Modify metadata adding

--- libraries/dl-streamer/src/monolithic/gst/elements/gvamotiondetect/gvamotiondetect.h
+++ libraries/dl-streamer/src/monolithic/gst/elements/gvamotiondetect/gvamotiondetect.h
@@ -10,7 +10,22 @@
 
 G_BEGIN_DECLS
 
+/* Forward declarations; struct is defined privately in the .cpp to allow
+ * internal fields without exposing them as public API. */
+typedef struct _GstGvaMotionDetect GstGvaMotionDetect;
+typedef struct _GstGvaMotionDetectClass GstGvaMotionDetectClass;
+
 #define GST_TYPE_GVA_MOTION_DETECT (gst_gva_motion_detect_get_type())
-G_DECLARE_FINAL_TYPE(GstGvaMotionDetect, gst_gva_motion_detect, GST, GVA_MOTION_DETECT, GstBaseTransform)
+GType gst_gva_motion_detect_get_type(void);
+
+/* Cast and type check macros (manual since we use forward declarations) */
+#define GST_GVA_MOTION_DETECT(obj) \
+	(G_TYPE_CHECK_INSTANCE_CAST((obj), GST_TYPE_GVA_MOTION_DETECT, GstGvaMotionDetect))
+#define GST_GVA_MOTION_DETECT_CLASS(klass) \
+	(G_TYPE_CHECK_CLASS_CAST((klass), GST_TYPE_GVA_MOTION_DETECT, GstGvaMotionDetectClass))
+#define GST_IS_GVA_MOTION_DETECT(obj) \
+	(G_TYPE_CHECK_INSTANCE_TYPE((obj), GST_TYPE_GVA_MOTION_DETECT))
+#define GST_IS_GVA_MOTION_DETECT_CLASS(klass) \
+	(G_TYPE_CHECK_CLASS_TYPE((klass), GST_TYPE_GVA_MOTION_DETECT))
 
 G_END_DECLS
