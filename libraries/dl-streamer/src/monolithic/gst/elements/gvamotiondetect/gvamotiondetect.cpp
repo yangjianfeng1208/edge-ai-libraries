12e000911c7ac2622fcc85c45c7209cc3f473462
Maciej Smiatacz <maciej.smiatacz@intel.com>
2025-11-21 10:54:21 +0100

    Add empty lines (CRITICAL)

--- libraries/dl-streamer/src/monolithic/gst/elements/gvamotiondetect/gvamotiondetect.cpp
+++ libraries/dl-streamer/src/monolithic/gst/elements/gvamotiondetect/gvamotiondetect.cpp
@@ -1094,3 +1094,4 @@ static gboolean plugin_init(GstPlugin *plugin) {
 
 GST_PLUGIN_DEFINE(GST_VERSION_MAJOR, GST_VERSION_MINOR, gvamotiondetect, PRODUCT_FULL_NAME " gvamotiondetect element",
                   plugin_init, PLUGIN_VERSION, PLUGIN_LICENSE, PACKAGE_NAME, GST_PACKAGE_ORIGIN)
+                  
